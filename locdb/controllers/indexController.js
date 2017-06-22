71b6376e29e0b90faaa0a0e694296d43afdf1e61
DESKTOP-JMEOF8R\baolo <baolocitplus@gmail.com>
2017-06-22 22:21:06 +0700

    no message

--- locdb/controllers/indexController.js
+++ locdb/controllers/indexController.js
@@ -1,14 +1,16 @@
-var express = require('express');
+var Product = require('../models/product')
 
+module.exports = {
+    getindex: function (req, res, next) {
+        Product.find(function (err, docs) {
+            var productChunks = [];
+            var chunkSize = 3;
+            for ( var i = 0; i < docs.length; i+= chunkSize){
+                productChunks.push(docs.slice(i,i + chunkSize));
+            }
+            res.render('shop/index', { title: 'Shopping Cart', products: docs });
+        });
+    }
 
-function getindex(req, res, next) {
-    Product.find(function (err, docs) {
-        var productChunks = [];
-        var chunkSize = 3;
-        for ( var i = 0; i < docs.length; i+= chunkSize){
-            productChunks.push(docs.slice(i,i + chunkSize));
-        }
-        res.render('shop/index', { title: 'Shopping Cart', products: docs });
-    });
 
 }
--- locdb/routes/index.js
+++ locdb/routes/index.js
@@ -1,17 +1,8 @@
 var express = require('express');
-var indexController = require('../controllers/indexController');
+var index = require('../controllers/indexController');
 var router = express.Router();
 var Product = require('../models/product');
 /* GET home page. */
-router.get('/',function (req, res, next) {
-    Product.find(function (err, docs) {
-        var productChunks = [];
-        var chunkSize = 3;
-        for ( var i = 0; i < docs.length; i+= chunkSize){
-            productChunks.push(docs.slice(i,i + chunkSize));
-        }
-        res.render('shop/index', { title: 'Shopping Cart', products: docs });
-    });
-});
+router.get('/',index.getindex);
 
 module.exports = router;
