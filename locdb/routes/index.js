07dbc17d3af97b436f95bb0e786bf4e2f47fa6c4
DESKTOP-JMEOF8R\baolo <baolocitplus@gmail.com>
2017-06-23 08:16:00 +0700

    no message

--- locdb/routes/index.js
+++ locdb/routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var index = require('../controllers/indexController');
-var router = express.Router();
+var user = require('../controllers/userController');
 var Product = require('../models/product');
+var csrf = require('csurf');
+var csrfProtection = csrf();
+var router = express.Router();
+router.use(csrfProtection);
 /* GET home page. */
 router.get('/',index.getindex);
-
+router.get('/user/signup', user.getsignup);
+router.post('user/signup', user.postsignup);
 module.exports = router;
