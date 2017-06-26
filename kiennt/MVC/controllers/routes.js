1392d5099ceb7efecf9e7752be3f24489209aa66
KienNguyen <kiennguyen@localhost.localdomain>
2017-06-26 21:58:50 +0700

    #No6 Update code new code Ajax and restructure code

--- kiennt/MVC/controllers/routes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const users = require('./users');
-
-router.post('/add', users.add);
--- kiennt/MVC/index.js
+++
@@ -1,7 +0,0 @@
-const port = process.env.PORT || 3000;
-const app = require('./controllers/config');
-/* istanbul ignore next */
-if (!module.parent) {
-    app.listen(3000);
-    console.log('Express started on port 3000');
-}
